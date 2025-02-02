2734e32bbec491a12a79c9fdbd7b8f987fb57ccd
Nazar Ivankiv <NIvankiv@slb.com>
2025-02-02 09:56:50 +0400

    solution

---
+++ 2_greedy_algorithm.py
@@ -0,0 +1,53 @@
+from model import Teacher, create_schedule
+
+
+if __name__ == "__main__":
+    # Множина предметів
+    subjects = {"Математика", "Фізика", "Хімія", "Інформатика", "Біологія"}
+
+    # Створення списку викладачів
+    teachers = [
+        Teacher(
+            "Олександр",
+            "Іваненко",
+            45,
+            "o.ivanenko@example.com",
+            {"Математика", "Фізика"},
+        ),
+        Teacher("Марія", "Петренко", 38, "m.petrenko@example.com", {"Хімія"}),
+        Teacher(
+            "Сергій",
+            "Коваленко",
+            50,
+            "s.kovalenko@example.com",
+            {"Інформатика", "Математика"},
+        ),
+        Teacher(
+            "Наталія", "Шевченко", 29, "n.shevchenko@example.com", {"Біологія", "Хімія"}
+        ),
+        Teacher(
+            "Дмитро",
+            "Бондаренко",
+            35,
+            "d.bondarenko@example.com",
+            {"Фізика", "Інформатика"},
+        ),
+        Teacher("Олена", "Гриценко", 42, "o.grytsenko@example.com", {"Біологія"}),
+    ]
+
+    # Виклик функції створення розкладу
+    schedule = create_schedule(subjects, teachers)
+
+    # Виведення розкладу
+    if schedule:
+        print("Розклад занять:")
+        for teacher in schedule:
+            print(
+                f"{teacher.first_name} {teacher.last_name}, {teacher.age} років, email: {teacher.email}"
+            )
+            if teacher.assigned_subjects:
+                print(f"   Викладає предмети: {', '.join(teacher.assigned_subjects)}\n")
+            else:
+                print("   Викладає предмети: немає призначених предметів\n")
+    else:
+        print("Неможливо покрити всі предмети наявними викладачами.")
